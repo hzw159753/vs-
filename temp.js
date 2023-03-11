09c3292a5a0d351028b4821705854f59588734dc
ZWH <3406992199@qq.com>
2023-03-11 09:37:22 +0800

    modified:   temp.js

---
+++ temp.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         网页复合功能
+// @namespace    EsfB2XVPmbThEv39bdxQR2hzid30iMF9
+// @version      0.1
+// @description  一些小功能：1、指定网页悬浮标志，快速跳转
+// @author       匿名
+// @include      *://*
+// @grant        none
+// @license      AGPL
+// ==/UserScript==
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
