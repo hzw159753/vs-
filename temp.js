045cc03b3cbaf95fa80db3eb2001591785118feb
ZWH <3406992199@qq.com>
2023-03-11 09:40:23 +0800

    modified:   temp.js

--- temp.js
+++ temp.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         网页复合功能
-// @namespace    EsfB2XVPmbThEv39bdxQR2hzid30iMF9
+// @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  一些小功能：1、指定网页悬浮标志，快速跳转
 // @author       匿名
