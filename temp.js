47ebd9798b12e6aa7caf8b920491d505657967a9
ZWH <3406992199@qq.com>
2023-03-11 09:53:02 +0800

    modified:   temp.js

--- temp.js
+++ temp.js
@@ -4,7 +4,8 @@
 // @version      0.1
 // @description  一些小功能：1、指定网页悬浮标志，快速跳转
 // @author       匿名
-// @include      *://*
+// @match        *://*
+// @icon         https://greasyfork.org/zh-CN/scripts/461609-%E7%BD%91%E9%A1%B5%E5%A4%8D%E5%90%88%E5%8A%9F%E8%83%BD
 // @grant        none
 // @license      AGPL
 // ==/UserScript==
